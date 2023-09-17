24a10f3d2f08d0a52d9b16358568bc3a9f78c64b
OlehKozubJS <122114245+OlehKozubJS@users.noreply.github.com>
2023-09-17 12:43:51 +0000

    Deploying to gh-pages from @ OlehKozubJS/react-blended-5@910406e33f1906a98e89e5baf8ecaffcbe5bb4de 🚀

---
+++ static/js/67.0dab4047.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[67],{67:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("h2",{children:"Rates"})}}}]);
+//# sourceMappingURL=67.0dab4047.chunk.js.map
